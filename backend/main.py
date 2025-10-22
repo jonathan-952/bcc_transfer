73fe4cdeb8325054c7897acd61fcebd1d95476d0
Jonathan <jonathan.s9522@gmail.com>
2025-10-22 12:47:54 -0400

    finished api endpoint, need to test next

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI
+import backend.db as db
+from contextlib import asynccontextmanager
+import os
+from dotenv import load_dotenv
+
 # api endpoints:
 #   fetch a degree by id
 #   fetch all requirements by degree id
 #   fetch courses by requirement id
 # 
 
-app = FastAPI()
+
+supabase = None
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    global supabase
+    load_dotenv()
+
+    db_url = os.getenv("DB_URL")
+    db_key = os.getenv('DB_KEY')
+
+    supabase = db.db_connection(db_url, db_key)
+    yield
+    
+        
+app = FastAPI(lifespan=lifespan)
 
 @app.get('/')
 async def main():
@@ -13,6 +33,6 @@ async def main():
 
 @app.get('/degree/${id}')
 async def get_degree(id: int):
-    # 
-    return
+    
+    return db.get_degree(supabase, id)
 
