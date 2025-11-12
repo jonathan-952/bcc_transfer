3989f516292230cb508f451ab9a5e51a180ba62e
Jonathan <jonathan.s9522@gmail.com>
2025-11-12 16:44:50 -0500

    update course matching, prob not gonna work :(

--- backend/course_matching.py
+++ backend/course_matching.py
@@ -7,6 +7,8 @@
 # unless course grade is less than C, label as unsure
 def course_match(requirements, transcript_courses):
     fulfilled_courses = []
+    unfulfilled_requirements = []
+    unfulfilled_group_requirements = []
     used_courses = []
     t_course_list = [element['course_code'] for element in transcript_courses if element['TR'] != 1]
 
@@ -20,17 +22,20 @@ def course_match(requirements, transcript_courses):
 
     cur_fulfilled = 0
     cur_group = 0 if not len(group_requirements) else group_requirements[0]['group_id']
+    last_group_count = None if not len(group_requirements) else group_requirements[-1]['group_count']
 
     # loop through group requirements
     for requirement in group_requirements:
 
-        if cur_fulfilled >= requirement['group_count'] and requirement['group_count'] == cur_group:
+        if cur_fulfilled >= requirement['group_count'] and requirement['group_id'] == cur_group:
             continue
-        
-        if cur_fulfilled >= requirement['group_count'] and requirement['group_count'] != cur_group:
-            cur_group = requirement['group_count']
-            cur_fulfilled = 0
 
+        if cur_fulfilled < requirement['group_count'] and requirement['group_id'] != cur_group:
+            unfulfilled_group_requirements.append(cur_group)
+
+        if requirement['group_id'] != cur_group:
+            cur_group = requirement['group_id']
+            cur_fulfilled = 0
 
         requirement_courses = [c.strip() for c in requirement['courses'].split(',')] if requirement['courses'] != None else []
 
@@ -41,14 +46,17 @@ def course_match(requirements, transcript_courses):
         
         matched_count = min(requirement['count'] or 0, len(matched_courses))
 
+        if matched_count:
+            fulfilled = (matched_courses)[:matched_count]
 
-        fulfilled = (matched_courses)[:matched_count]
+            used_courses += fulfilled
 
-        used_courses += fulfilled
+            fulfilled_courses += fulfilled
 
-        fulfilled_courses += fulfilled
+            cur_fulfilled += 1
 
-        cur_fulfilled += 1
+    if cur_group and last_group_count and cur_fulfilled < last_group_count:
+        unfulfilled_group_requirements.append(cur_group)
 
 
     # logic for solo requirements stays the same
@@ -63,6 +71,9 @@ def course_match(requirements, transcript_courses):
 
         matched_count = min(requirement['count'] or 0, len(matched_courses))
 
+        if not matched_count:
+            unfulfilled_requirements.append(requirement['requirement_id'])
+
 
         fulfilled = (matched_courses)[:matched_count]
 
@@ -72,7 +83,9 @@ def course_match(requirements, transcript_courses):
     return {
         'courses' : fulfilled_courses,
         # label as review if either transfer or not directly in requirements, can still fulfill elective
-        'review' : [element['course_code'] for element in transcript_courses if element['TR'] == 1 or element['course_code'] not in fulfilled_courses]
+        'review' : [element['course_code'] for element in transcript_courses if element['TR'] == 1 or element['course_code'] not in fulfilled_courses],
+        'unfulfilled' : unfulfilled_requirements,
+        'unfulfilled_group' : unfulfilled_group_requirements
     }
 
 # 2 arrays: grouped requirements and solo
